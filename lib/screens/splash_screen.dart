d4a1f60fcf746c58a28ce8a29f25ca052633d787
Otto <ottosantoso11@gmail.com>
2024-06-28 17:43:50 +0700

    animasi lottie

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,22 +10,46 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 5), () {
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    Timer(Duration(seconds: 4), () {
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: ((context) => const MyLogin())));
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Lottie.asset("images/logo_video.json", height: 50),
+        child: ScaleTransition(
+          scale: Tween(begin: 1.0, end: 1.5).animate(CurvedAnimation(
+            parent: _controller,
+            curve: Curves.easeInOut,
+          )),
+          child: Lottie.asset(
+            "images/logo_video.json",
+            width: 200,
+            height: 200,
+          ),
+        ),
       ),
     );
   }
