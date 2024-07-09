f77d8bf35df31aef4af683cd6e80e6fd57539633
Otto <ottosantoso11@gmail.com>
2024-07-09 15:43:54 +0700

    fixxing bottom overflowed

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -5,7 +5,7 @@ import 'package:projectuts/screens/signup_page.dart';
 import 'package:projectuts/screens/home_screen.dart';
 import 'package:projectuts/widgets/button.dart';
 import 'package:flutter/material.dart';
-import 'package:projectuts/widgets/textfield.dart'; 
+import 'package:projectuts/widgets/textfield.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -31,55 +31,57 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-          children: [
-            const Spacer(),
-            const Text(
-              "Selamat Datang di Aplikasi Transportasi Kampus",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(height: 50),
-            CustomTextField(
-              hint: "Enter Email",
-              label: "Email",
-              controller: _email,
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              hint: "Enter Password",
-              label: "Password",
-              controller: _password,
-              obscureText: !_passwordVisible,
-              toggleVisibility: () {
-                setState(() {
-                  _passwordVisible = !_passwordVisible;
-                });
-              },
-              isPassword: true,
-            ),
-            const SizedBox(height: 30),
-            CustomButton(
-              label: "Login",
-              onPressed: _login,
-            ),
-            const SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Belum memiliki akun? "),
-                InkWell(
-                  onTap: () => goToSignup(context),
-                  child: const Text(
-                    "Signup",
-                    style: TextStyle(color: Colors.red),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              const Text(
+                "Selamat Datang di Aplikasi Transportasi Kampus",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 50),
+              CustomTextField(
+                hint: "Enter Email",
+                label: "Email",
+                controller: _email,
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                hint: "Enter Password",
+                label: "Password",
+                controller: _password,
+                obscureText: !_passwordVisible,
+                toggleVisibility: () {
+                  setState(() {
+                    _passwordVisible = !_passwordVisible;
+                  });
+                },
+                isPassword: true,
+              ),
+              const SizedBox(height: 30),
+              CustomButton(
+                label: "Login",
+                onPressed: _login,
+              ),
+              const SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Belum memiliki akun? "),
+                  InkWell(
+                    onTap: () => goToSignup(context),
+                    child: const Text(
+                      "Signup",
+                      style: TextStyle(color: Colors.red),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const Spacer(),
-          ],
+                ],
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
